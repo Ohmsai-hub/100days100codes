7058de8b420f9192c6484f182c8b062ad937ec83
Ohmsai <ohmsainirmalkar@gmail.com>
2025-08-22 07:44:58 +0000

    Finding area and Circumference of circle

---
+++ oponcircle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main() {
+char command[10];
+int radius , area , circumference ;
+printf("Enter the radius of the circle: ");
+scanf("%d", &radius);
+area = 3.14*radius*radius;
+circumference = 2*3.14*radius;
+printf("Area or Circumference: ");
+scanf("%s", command);
+if (strcmp(command, "Circumference") == 0){
+printf("The Circumference of the Circle is : %d", circumference);
+}
+else if (strcmp(command, "Area") ==0){
+printf("The Area of the circle is : %d", area);
+}
+else{
+printf("Invalid Command");
+}
+return 0;
+}
+
