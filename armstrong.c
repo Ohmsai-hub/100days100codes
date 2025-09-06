caff43d9c787df962276204d3141eca2bb757e53
Ohmsai <ohmsainirmalkar@gmail.com>
2025-09-06 22:15:02 +0530

    Create armstrong.c
    Program to check if a number is an Armstrong  number

---
+++ armstrong.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, original, remainder, n = 0;
+    double result = 0.0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    // count number of digits
+    while (original != 0) {
+        original /= 10;
+        n++;
+    }
+
+    original = num;
+
+    // calculate sum of digits^n
+    while (original != 0) {
+        remainder = original % 10;
+        result += pow(remainder, n);
+        original /= 10;
+    }
+
+    // check Armstrong
+    if ((int)result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
