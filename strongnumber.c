7bf5c5a2e98d23dbcbd4c2dfef92af4d62ff3add
Ohmsai <ohmsainirmalkar@gmail.com>
2025-09-12 22:57:09 +0530

    Create strongnumber.c
    to check if a number is strong number or not

---
+++ strongnumber.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Function to calculate factorial of a digit
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num, temp, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;  // keep a copy
+
+    while (temp > 0) {
+        digit = temp % 10;          // extract last digit
+        sum += factorial(digit);    // add factorial of digit
+        temp /= 10;                 // remove last digit
+    }
+
+    if (sum == num)
+        printf("%d is a Strong Number.\n", num);
+    else
+        printf("%d is NOT a Strong Number.\n", num);
+
+    return 0;
+}
