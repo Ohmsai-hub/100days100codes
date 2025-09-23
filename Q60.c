5e905fa496411a0f10d1151e7762c6216e47e4e8
Ohmsai <ohmsainirmalkar@gmail.com>
2025-09-23 05:58:08 +0000

    Create Q60.c
    Question 60

---
+++ Q60.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    int pos = 0, neg = 0, zero = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {
+        if (arr[i] > 0)
+            pos++;
+        else if (arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+    printf("Count of positive numbers = %d\n", pos);
+    printf("Count of negative numbers = %d\n", neg);
+    printf("Count of zeroes = %d\n", zero);
+    return 0;
+}
