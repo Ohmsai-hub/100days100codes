e0de2c2669b6b86abe9c29374d92dde875e93645
Ohmsai <ohmsainirmalkar@gmail.com>
2025-09-23 03:42:24 +0000

    Create Q52.c
    Question 52

---
+++ Q52.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int rows[5] ={1,3,5,3,1};
+int i,j,k;
+    for(i=0;i<5;i++){
+        for(j=0;j<rows[i];j++){
+           printf("*\n");
+        }
+        printf("\n");
+    }
+return 0;
+}
